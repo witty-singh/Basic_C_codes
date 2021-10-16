bcb00bcd24384baee2c17ba2b3b1cb0eadc9e604
witty-singh <87194791+witty-singh@users.noreply.github.com>
2021-10-16 10:01:24 +0530

    Create quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -2,11 +2,11 @@
 #include<math.h>
 int main()
 {
-    float j1,j2,sqD,D,a,b,c; //sqD is  floating value of square root of D//
+    double j1,j2,sqD,D,a,b,c; //sqD is  floating value of square root of D//
     printf("enter the values of a b and c of a quardratic eqn.\n");
-    scanf("%f %f %f", &a,&b,&c);
-    printf("your eqn is : \n%0.1fx^2+%0.1fx+%0.1f\n", a,b,c);
-    D=b*b-4*a*c;
+    scanf("%lf %lf %lf", &a,&b,&c);
+    printf("your eqn is : \n%0.1lfx^2+%0.1lfx+%0.1lf\n", a,b,c);
+    D=(b*b)-(4*a*c);
     sqD=sqrt(D);
     if (D<0)
     {
@@ -18,7 +18,7 @@ int main()
        j1=(-b+sqD)/2*a;
        j2=(-b-sqD)/2*a;
  
-       printf("roots of the given eqn. are %f,%f", j1,j2);
+       printf("roots of the given eqn. are %lf,%lf", j1,j2);
     }
     
 return 0;
